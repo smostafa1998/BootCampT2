b23eb8f497715ab169c0aefaa548e4e694a8152d
Sabreen <smostafa1998@gmail.com>
2021-09-29 15:32:05 -0400

    bmw 6 cases

--- com.BMWUSA/src/main/java/pom/ShoppingHomePage.java
+++ com.BMWUSA/src/main/java/pom/ShoppingHomePage.java
@@ -46,10 +46,50 @@ public class ShoppingHomePage extends BaseClass {
     @FindBy(xpath="//div[2]/div[1]//div[1]//div[2]/div[2]/div[2]/button")
     public WebElement estimatePayments;
 
-    @FindBy(xpath="//div[2]/div[1]//div[1]//div[1]/div[3]/div[2]/div[3]/p")
-    public WebElement confirmPrice;
+    @FindBy(xpath="//div[2]/div[1]//div[1]//section/div[1]/div[3]/div[2]/div[1]/p")
+    public WebElement confirmYear;
 
+    @FindBy(xpath="//div[1]//div[5]/div[2]//div[2]/div[2]/nav[1]/ul/li[4]/a")
+    public WebElement shopPartsTab;
 
+    @FindBy(xpath="//*[@id=\"divTopCategories\"]/div/div/a/div/p")
+    public List<WebElement> listAccessories;
+
+    @FindBy(xpath="//*[@id=\"divTopCategories\"]/div[3]/div/a")
+    public WebElement bmwApparel;
+
+    @FindBy(xpath="//*[@id=\"btnSort\"]")
+    public WebElement buttonSort;
+
+    @FindBy(xpath="//*[@id=\"description2\"]/div/ul/li[4]/a")
+    public WebElement lowTohigh;
+
+    @FindBy(xpath="//*[@id=\"accordian\"]/div[2]/div/div[1]//div[3]//a")
+    public WebElement apparalTab;
+
+    @FindBy(xpath="//*[@id=\"accordian\"]/div[2]//div[1]//div[3]//a")
+    public WebElement kidTab;
+
+    @FindBy(xpath = "//*[@id=\"divPrdListMain\"]/div/div[3]/div/div/div[2]/a")
+    public List<WebElement> kidItems;
+
+    @FindBy(xpath="//div[1]//div[5]/div[2]//div[2]/div[2]/nav[2]/ul/li[1]/a")
+    public WebElement leaseAndFinancing;
+
+    @FindBy(xpath="//div[2]/div[1]//div[3]/div/bmw-template-switch//form/div[1]/div[1]//input")
+    public WebElement zipCodeInput;
+
+    @FindBy(xpath="//div[2]/div[1]//div[3]//bmw-template-switch//div[2]/div/select")
+    public WebElement vehicleSelect;
+
+    @FindBy(xpath="//div[2]/div[1]//div[3]//bmw-template-switch//form/div[4]/div/select")
+    public WebElement yearsSelect;
+
+    @FindBy(xpath="//div[3]//bmw-template-switch[2]//ul/li[2]/div/div[1]/div[1]/div")
+    public WebElement moreInfo;
+
+    @FindBy(xpath="//div[2]//div[3]//bmw-template-switch[2]//ul/li[2]/div//div[2]/div[1]/ul/li")
+    public List<WebElement> infoList;
 
 
 
--- com.BMWUSA/src/test/java/testPom/TestShoppingHomePage.java
+++ com.BMWUSA/src/test/java/testPom/TestShoppingHomePage.java
@@ -1,5 +1,6 @@
 package testPom;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pom.Homepage;
 import pom.ShoppingHomePage;
@@ -24,40 +25,87 @@ public class TestShoppingHomePage extends TestBase {
         ShoppingHomePage shopping = homepage.navigateToShoppingPage();
         clickOnElement(shopping.zipCode);
         clearInputText(shopping.zipCode);
-        sendKeysToInput(shopping.zipCode,"11377");
+        sendKeysToInput(shopping.zipCode, "11377");
         clickOnElement(shopping.shopNew);
         List<String> elementCopied = new ArrayList<>();
         getListOfElements(shopping.pricesNewCars, elementCopied);
-        database.insertDataFromListToSqlTable(elementCopied, "NewCar11377","prices");
+        database.insertDataFromListToSqlTable(elementCopied, "NewCar11377", "prices");
     }
 
-    @Test()
+    @Test(enabled = false)
     public void verifyShoppingTest3() {
         Homepage homepage = getHomepage();
         ShoppingHomePage shopping = homepage.navigateToShoppingPage();
         clickOnElement(shopping.estimateButton);
 
         clickOnElement(shopping.series);
-        dropdownSelectByIndex(shopping.series,3);
+        dropdownSelectByIndex(shopping.series, 3);
 
         clickOnElement(shopping.model);
-        dropdownSelectByIndex(shopping.model,4);
+        dropdownSelectByIndex(shopping.model, 4);
 
         clickOnElement(shopping.inputZIP);
         clearInputText(shopping.inputZIP);
-        sendKeysToInput(shopping.inputZIP,"11377");
+        sendKeysToInput(shopping.inputZIP, "11377");
 
         clickOnElement(shopping.estimatePayments);
-        waitForElementToBeVisible(shopping.confirmPrice);
+        waitForElementToBeVisible(shopping.confirmYear);
+        String actualText = shopping.confirmYear.getText();
+        String expectedText = "2022";
+        Assert.assertEquals(actualText, expectedText);
+    }
 
-        /*
-        clickOnElement(shopping.downPaymentInput);
-        clearInputText(shopping.downPaymentInput);
-        sendKeysToInput(shopping.downPaymentInput,"3000");
+    @Test(enabled = false)
+    public void verifyShoppingTest4() {
+        Homepage homepage = getHomepage();
+        ShoppingHomePage shopping = homepage.navigateToShoppingPage();
+        clickOnElement(shopping.shopPartsTab);
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+        List<String> elementCopied = new ArrayList<>();
+        getListOfElements(shopping.listAccessories, elementCopied);
+        driver.close();
+        driver.switchTo().window(tabs2.get(0));
+    }
 
-        clickOnElement(shopping.termSelect);
-        dropdownSelectByIndex(shopping.termSelect,2);
-        */
+    @Test(enabled = false)
+    public void verifyShoppingTest5() {
+        Homepage homepage = getHomepage();
+        ShoppingHomePage shopping = homepage.navigateToShoppingPage();
+        clickOnElement(shopping.shopPartsTab);
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+        clickOnElement(shopping.bmwApparel);
+        clickOnElement(shopping.apparalTab);
+        clickOnElement(shopping.kidTab);
+        clickOnElement(shopping.buttonSort);
+        clickOnElement(shopping.lowTohigh);
+        List<String> elementCopied = new ArrayList<>();
+        getListOfElements(shopping.kidItems, elementCopied);
+        database.insertDataFromListToSqlTable(elementCopied, "BMWKids", "items");
+        driver.close();
+        driver.switchTo().window(tabs2.get(0));
+    }
+
+    @Test(enabled = false)
+    public void verifyShoppingTest6() {
+        Homepage homepage = getHomepage();
+        ShoppingHomePage shopping = homepage.navigateToShoppingPage();
+        clickOnElement(shopping.leaseAndFinancing);
+
+        clickOnElement(shopping.zipCodeInput);
+        clearInputText(shopping.zipCodeInput);
+        sendKeysToInput(shopping.zipCodeInput, "11377");
+
+        clickOnElement(shopping.vehicleSelect);
+        dropdownSelectByIndex(shopping.vehicleSelect, 4);
+
+        clickOnElement(shopping.yearsSelect);
+        dropdownSelectByIndex(shopping.yearsSelect, 2);
+
+        clickOnElement(shopping.moreInfo);
+        List<String> elementCopied = new ArrayList<>();
+        getListOfElements(shopping.infoList, elementCopied);
 
     }
 
